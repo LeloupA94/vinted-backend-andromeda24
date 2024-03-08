e6ec24156a2f3a06dc20976961a228276a0293a2
antoine leloup <al@MacBook-Air.local>
2024-03-08 11:57:18 +0100

    rajout package cors et init cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+app.use(cors());
 app.use(express.json());
 const fileUpload = require("express-fileupload");
 const cloudinary = require("cloudinary").v2;
